142fcf6ac955ded93cf01852eeba3fae528df3e0
bulat nitaliev <bulatnitaliev2016@gmail.com>
2024-05-21 18:02:59 +0400

    me

--- testogram/general/api/views.py
+++ testogram/general/api/views.py
@@ -1,16 +1,27 @@
-from general.api.serializer import UserRegisterationSerializer, UserListSerializer
+from general.api.serializer import UserRegisterationSerializer, UserListSerializer, UserRetrieveSerializer
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 from general.models import User
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-class UserViewSet(CreateModelMixin,ListModelMixin, GenericViewSet):
+class UserViewSet(CreateModelMixin,ListModelMixin,RetrieveModelMixin, GenericViewSet):
   queryset = User.objects.all().order_by('-id')
   permission_classes = [IsAuthenticated]
+
+  
+  @action(detail=False, methods=["get"], url_path="me")
+  def me(self,request):
+    isinstance = self.request.user
+    serializer = self.get_serializer(isinstance)
+    return Response(serializer.data)
 
   def get_serializer_class(self):
     if self.action == 'create':
       return UserRegisterationSerializer
+    if self.action in ['retrieve', 'me']:
+      return UserRetrieveSerializer
     return UserListSerializer
 
   def get_permissions(self):
